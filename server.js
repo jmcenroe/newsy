275b33468ada08d674e91748d638d77f5adbcb57
Jarrod <jwmcenroe@gmail.com>
2018-01-12 16:25:24 -0800

    Adding handlebars files

--- server.js
+++ server.js
@@ -1,31 +1,7 @@
-// Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var cheerio = require("cheerio");
+'use strict';
 
-var request = require("request");
+const childProcess = require('child_process');
 
-mongoose.Promise = global.Promise;
-
-var app = express();
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-app.use(express.static(__dirname + "/public"));
-
-var exphbs = require("express-handlebars");
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-mongoose.connect('mongodb://localhost/product')
-  .then(() =>  console.log('mongoose connection succesful'))
-  .catch((err) => console.error(err));
-
-// Listen on port 3000
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
+const process = childProcess.fork('./bin/www', {
+    stdio: 'inherit'
 });
